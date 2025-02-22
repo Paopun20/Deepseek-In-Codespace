d5e09a954b5e9412a926def56ac99ebb3da80652
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2025-02-22 09:09:02 +0000

    main

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # sh code
 # setup.sh
 sudo apt update -y
-sudo apt install -y glances
-curl -fsSL https://ollama.com/install.sh | sh
 sudo yum install -y python3 python3-pip
 python3 -m pip install --upgrade pip
+sudo apt install -y glances
+curl -fsSL https://ollama.com/install.sh | sh
 pip3 install flask ngrok
